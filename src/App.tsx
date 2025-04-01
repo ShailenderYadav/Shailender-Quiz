f52359115311cc9a0a4118cf07ded1aeb09008a7
Shailender Yadav <39689169-shailender12131@users.noreply.replit.com>
2025-04-01 08:30:25 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { getQuizzes } from './data/quizzes';
+import './App.css'
+
+export default function App() {
+  return (
+    <main>
+     App
+    </main>
+  )
+}
